ac298d0425acbb95916a8a9060a381b05a315421
Scott Agnew <sesquipedalian.dev@gmail.com>
2021-08-22 16:56:53 -0600

    rename crates from week_x to day_x

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Advent of Code 2020
 
 ## Summary
-Implementation of advent of code challenges in rust.  Each week's challenge is a separate crate, with public methods for the 2 challenges, a binary runner, and my input(s) as an example.
+Implementation of advent of code challenges in rust.  Each day's challenge is a separate crate, with public methods for the 2 challenges, a binary runner, and my input(s) as an example.
 
--- advent_2020_day_1/src/main.rs
+++ advent_2020_day_1/src/main.rs
@@ -1,11 +1,11 @@
 use advent_2020_common::{input, Error};
-use advent_2020_week_2::{first, second};
+use advent_2020_day_1::{first, second};
 
 fn main() -> Result<(), Error> {
     let input = input().or(Error::new("Couldn't read input file"))?;
     println!("Using puzzle input {:?}", input);
 
-    println!("First output: {}", first(&input).unwrap());
-    println!("Second output: {}", second(&input).unwrap());
+    println!("First output: {}", first(&input));
+    println!("Second output: {}", second(&input));
     Ok(())
 }
