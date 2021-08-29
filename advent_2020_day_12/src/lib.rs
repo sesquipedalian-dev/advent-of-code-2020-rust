0b641886ff8daa95de5758333672b6ed0990f62e
Scott Agnew <sesquipedalian.dev@gmail.com>
2021-08-29 12:16:08 -0600

    day 12 p2

--- advent_2020_day_12/src/lib.rs
+++ advent_2020_day_12/src/lib.rs
@@ -4,10 +4,10 @@ use std::convert::TryFrom;
 
 #[derive(Clone, Debug, PartialEq, Copy)]
 enum Directions {
-    East = 0,
-    South,
-    West,
-    North
+    East = 0, // 00
+    South, // 01
+    West, // 10
+    North // 11
 }
 
 impl TryFrom<u32> for Directions {
@@ -68,6 +68,66 @@ pub fn first(input: &[String]) -> Result<u32, Error> {
     )
 }
 
+fn rotate_point(x: i32, y: i32, rotation: u32 /*1, 2, 3 = 90, 180, 270*/) -> Result<(i32, i32), Error> {
+    let sin: i32 = match 2 - (rotation - 1) {
+        0 => 1,
+        1 => 0,
+        2 => -1,
+        _ => return Error::new("invalid rotation - should be 1-3 = 90-270 *")
+    };
+    let cosine: i32 = match 2 - (rotation - 1) {
+        0 => 0,
+        1 => -1,
+        2 => 0,
+        _ => return Error::new("invalid rotation - should be 1-3 = 90-270 *")
+    };
+
+    Ok((
+        (x * cosine) - (y * sin),
+        (x * sin) + (y * cosine),
+    ))
+}
+
+pub fn second(input: &[String]) -> Result<u32, Error> {
+    let instruction_regex = Regex::new("^(N|S|E|W|L|R|F)(\\d+)$").or(Error::new("Couldn't compile regex!"))?;
+    let mut direction_mods: Vec<i32> = vec!(10, 1); // waypoint starts 10 east 1 north
+    let (mut current_x, mut current_y) = (0, 0);
+
+    for line in input {
+        let caps = match instruction_regex.captures(line) {
+            Some(captures) => captures,
+            _ => return Error::from_string(format!("invalid line {}", line)),
+        };
+
+        let amount: i32 = caps[2].parse().or(Error::new("line missing number"))?;
+        match &caps[1] {
+            "N" => direction_mods[Directions::South as usize] += amount,
+            "S" => direction_mods[Directions::South as usize] -= amount,
+            "E" => direction_mods[Directions::East as usize] += amount,
+            "W" => direction_mods[Directions::East as usize] -= amount,
+            "F" => {
+                current_y += amount * direction_mods[Directions::South as usize];
+                current_x += amount * direction_mods[Directions::East as usize];
+            },
+            "L" => {
+                let degree_90_turns = 3 - ((amount / 90) - 1) as u32;
+                let (new_x, new_y) = rotate_point(direction_mods[0], direction_mods[1], degree_90_turns)?;
+                direction_mods[Directions::East as usize] = new_x;
+                direction_mods[Directions::South as usize] = new_y;
+            },
+            "R" => {
+                let degree_90_turns = (amount / 90) as u32;
+                let (new_x, new_y) = rotate_point(direction_mods[0], direction_mods[1], degree_90_turns)?;
+                direction_mods[Directions::East as usize] = new_x;
+                direction_mods[Directions::South as usize] = new_y;
+            },
+            x => return Error::from_string(format!("invalid instruction! {}", x)),
+        };
+    }
+    
+    Ok(current_y.abs() as u32 + current_x.abs() as u32)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -86,5 +146,18 @@ mod tests {
     fn test_first() {
         let result = first(&example()).unwrap();
         assert_eq!(result, 25);
+    }
+
+    #[test]
+    fn test_second() {
+        let result = second(&example()).unwrap();
+        assert_eq!(result, 286);
+    }
+
+    #[test]
+    fn test_rotate() {
+        let (east, north) = rotate_point(10, 4, 1).unwrap();
+        assert_eq!(east, 4);
+        assert_eq!(north, -10);
     }
 }
